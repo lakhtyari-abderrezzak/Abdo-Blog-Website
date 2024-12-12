9ef56f9da20bed8821bd38ea9ee5a20d72461aad
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2024-12-12 23:47:06 +0000

    Cashed Categories Links For they Wont be Changing Much

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,15 +5,22 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Cache;
 
 class PostController extends Controller
 {
     public function index(){
-        return view('posts.index',[
-            'categories' => Category::take(10)
+
+        $categories = Cache::remember('posts-categories', Carbon::now()->addDay(), function(){
+            return Category::take(10)
             ->whereHas('posts', function($query){
                     $query->published();
-            })->get()
+            })->get();
+        });
+
+        return view('posts.index',[
+            'categories' => $categories,
         ]);
     }
     public function show(Post $post){
