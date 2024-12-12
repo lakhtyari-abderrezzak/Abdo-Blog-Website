49278aff3f185ec99022630d0fc8ba0e53e48c83
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2024-12-12 23:45:26 +0000

    Cached featured Posts and Latest Posts for a Day

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
@@ -11,11 +13,17 @@ class HomeController extends Controller
      * Handle the incoming request.
      */
     public function __invoke(Request $request)
-    {
+    {   
+        $featuredPost = Cache::remember('featuredPost', Carbon::now()->addDay(), function () {
+           return Post::published()->with('categories')->featured()->latest('published_at')->take(3)->get();
+        });
+        $latestPost = Cache::remember('latestPost', Carbon::now()->addDay(), function(){
+            return Post::published()->with('categories')->latest('published_at')->take(9)->get();
+        });
         return view('home',
     [
-        'featuredPost' => Post::published()->with('categories')->featured()->latest('published_at')->take(3)->get(),
-        'latestPost' => Post::published()->with('categories')->latest('published_at')->take(9)->get(),
+        'featuredPost' => $featuredPost,
+        'latestPost' => $latestPost
     ]);
     }
 }
