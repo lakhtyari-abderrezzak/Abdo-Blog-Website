65c246470e2d9b442039682aef4581b71cfbf6da
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2024-12-16 12:31:22 +0000

    added profile_lang file to en ar anf fr

---
+++ lang/en/profile_lang.php
@@ -0,0 +1,20 @@
+<?php 
+
+return [
+    'profile_info' => 'Profile Information',
+    'profile_update' => "Update your account's profile information and email address.",
+    'photo' => 'Photo',
+    'select_photo' => 'Select a New Photo',
+    'remove_photo' => 'Remove Photo',
+    'name' => 'Name',
+    'email' => 'Email',
+    'save' => 'Save',
+    'saved' => 'Saved.',
+
+    // password Update
+    'update_pass' => 'Update Paassword',
+    'pass_desc' => 'Ensure your account is using a long, random password to stay secure.',
+    'current' => 'Current Password',
+    'new' => 'New Password',
+    'confirm' => 'Confirm Password',
+];
---
+++ lang/fr/profile_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'profile_info' => 'Informations du profil',
+    'profile_update' => 'Mettez à jour les informations de votre compte et votre adresse e-mail.',
+    'photo' => 'Photo',
+    'select_photo' => 'Sélectionner une nouvelle photo',
+    'remove_photo' => 'Supprimer la photo',
+    'name' => 'Nom',
+    'email' => 'E-mail',
+    'save' => 'Sauvegarder',
+    'saved' => 'Enregistré.',
+
+    // password Update
+    'update_pass' => 'Mettre à jour le mot de passe',
+    'pass_desc' => 'Assurez-vous que votre compte utilise un mot de passe long et aléatoire pour rester sécurisé.',
+    'current' => 'Mot de passe actuel',
+    'new' => 'Nouveau mot de passe',
+    'confirm' => 'Confirmer le mot de passe',
+];
