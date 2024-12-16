798318260d871d502e8c52aaa00d69312f2df1eb
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2024-12-16 11:30:38 +0000

    Small changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,14 +28,14 @@
             <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-checkbox id="remember_me" name="remember" />
-                    <span class="ms-2 text-sm text-gray-600">{{ __('loging_lang_remember_me') }}</span>
+                    <span class="ms-2 text-sm text-gray-600">{{ __('login_lang.remember_me') }}</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('login_lang.forgot?') }}
+                        {{ __('login_lang.forgot') }}
                     </a>
                 @endif
 
